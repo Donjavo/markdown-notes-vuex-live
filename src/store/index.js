ec21d33abc241c077f8b9bda93459724fa582cf6
DonJavo <javiermartinex14@gmail.com>
2021-05-31 17:15:57 -0500

    Creamos el store para obtener el valor actual del state de las notas

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    notes: []
   },
   mutations: {
+    setNotes (state, notes) {
+      state.notes = notes
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
