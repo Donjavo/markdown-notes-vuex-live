7335064179854bae2981239f1b473a3bddb79284
DonJavo <javiermartinex14@gmail.com>
2021-05-31 17:15:18 -0500

    Creamos componentes para cada item nota y el contenedor principal de dichas notas

---
+++ src/components/NoteList.vue
@@ -0,0 +1,19 @@
+<template>
+      <div>
+          <note-list-item v-for="note in notes" :key="note.id" :note="note" />
+      </div>
+</template>
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import NoteListItem from './NoteListItem.vue'
+export default {
+  components: { NoteListItem },
+  name: 'NoteList',
+  setup () {
+    const store = useStore()
+    // const notes = computed(() => store.state.notes)
+    return { notes: computed(() => store.state.notes) }
+  }
+}
+</script>
